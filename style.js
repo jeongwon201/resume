2326095fd1e6a70717c778891ef31a2c107f843a
jeongwon <jeongwon201@naver.com>
2023-08-01 19:54:06 +0900

    @update design, content

--- style.js
+++ style.js
@@ -1,42 +1,91 @@
-// 스킬 탭
-var defaultOpen = document.getElementsByClassName("default-open");
-for(var i=0; i<defaultOpen.length; i++) {
-  defaultOpen[i].click();
-}
+$.fn.commentCards = function () {
+  return this.each(function () {
+    var $this = $(this),
+      $cards = $this.find('.card'),
+      $current = $cards.filter('.card--current'),
+      $next;
 
-function openTab(evt, skill, category) {
-  var j, tablinks, tabcontent;
-  
-  tabcontent = document.getElementsByClassName("tabcontent " + category);
-  for (j = 0; j < tabcontent.length; j++) {
-    tabcontent[j].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks " + category);
-  for (j = 0; j < tablinks.length; j++) {
-    tablinks[j].className = tablinks[j].className.replace(" active", "");
-  }
-  document.getElementById(skill).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+    $cards.on('click', function () {
+      if (!$current.is(this)) {
+        const offset = $('section').first().offset();
+        $('html').animate({ scrollTop: offset.top - 100 }, 200);
+        $cards.removeClass('card--current card--out card--next card--up');
+        $current = $(this).addClass('card--current');
+        $next = $current.next();
+        $next = $next.length ? $next.addClass('card--next') : $cards.first().addClass('card--next');
+        $next.next().length ? $next.next().addClass('card--out') : $cards.first().addClass('card--out');
+      } else {
+        if ($('.card--up').is(this)) {
+          $next = $cards.filter('.card--next');
+          const offset = $('section').first().offset();
+          $('html').animate({ scrollTop: offset.top - 100 }, 200);
+          $cards.removeClass('card--current card--out card--next card--up');
+          $current = $next.addClass('card--current');
+          $next = $current.next().length ? $current.next().addClass('card--next') : $cards.first().addClass('card--next');
+          $next.next().length ? $next.next().addClass('card--out') : $cards.first().addClass('card--out');
+        }
+      }
+    });
 
+    $('container > footer').hover(
+      function () {
+        $('#snackbar').html('다음 페이지');
+        $current.addClass('card--up');
+        $('#snackbar').removeClass('hide');
+        $('#snackbar').addClass('show');
+      },
+      function () {
+        $current.removeClass('card--up');
+        $('#snackbar').removeClass('show');
+        $('#snackbar').addClass('hide');
+      },
+    );
 
-// 자세히 버튼
-var hs = document.getElementsByClassName("hs");
-for (i = 0; i < hs.length; i++) {
-  hs[i].addEventListener("click", function() {
-    var detail = this.previousElementSibling;
-    var simple = detail.previousElementSibling;
-
-    if(detail.style.display=="block") {
-      detail.style.display="none";
-      simple.style.display="block";
-      this.textContent = "자세히";
-      var location = simple.offsetTop - 300;
-      window.scrollTo({top:location});
-    } else {
-      detail.style.display="block";
-      simple.style.display="none";
-      this.textContent = "간략히";
+    if (!$current.length) {
+      $current = $cards.last();
+      $cards.first().trigger('click');
     }
+
+    $this.addClass('cards--active');
   });
-}
+};
+
+$('.cards').commentCards();
+
+/** Second Page */
+$('.detail').fadeOut();
+$('.simple').click(function () {
+  var $detail = $(this).next();
+  if ($detail.css('display') == 'none') {
+    $('.detail').fadeOut();
+    $detail.fadeIn();
+  }
+});
+
+$('.simple').hover(
+  function () {
+    var $details = $('.detail'),
+      $flag = false,
+      i;
+
+    for (i = 0; i < $details.length; i++) {
+      if ($($details[i]).css('display') == 'block') {
+        $flag = true;
+      }
+    }
+
+    if ($flag == false) {
+      $('#snackbar').html('자세히 보기');
+      $('#snackbar').removeClass('hide');
+      $('#snackbar').addClass('show');
+    }
+  },
+  function () {
+    $('#snackbar').removeClass('show');
+    $('#snackbar').addClass('hide');
+  },
+);
+
+$('.close').click(function () {
+  $('.detail').fadeOut();
+});
